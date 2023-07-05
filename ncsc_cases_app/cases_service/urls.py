5ea37da39878b7840a59a140525a06da711b0c83
haarismian <mail@haarismian.com>
2023-07-05 17:43:40 -0400

    create case functionality done

--- ncsc_cases_app/cases_service/urls.py
+++ ncsc_cases_app/cases_service/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from . import views
-from .views import CaseListView, delete_case
+from .views import CaseListView, CaseCreateView
 
 app_name = "cases"
 urlpatterns = [
     path("", views.CaseListView.as_view(), name='case_list'),
-    path('cases/<str:id>/delete/', views.delete_case, name='delete_case'),
+    path('<str:id>/delete/', views.delete_case, name='delete_case'),
+    path('create/', views.CaseCreateView.as_view(), name='create_case'),
+
 
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
--- ncsc_cases_app/cases_service/views.py
+++ ncsc_cases_app/cases_service/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.utils import timezone
+from django.views.generic.edit import CreateView
+
 
 from django.views.decorators.http import require_POST
 
@@ -11,26 +13,19 @@ from .models import Question, Choice, Case
 
 class CaseListView(generic.ListView):
     model = Case
-    template_name = 'case_list.html'
+    template_name = 'cases_service/case_list.html'
     context_object_name = 'cases'
 
     def get_queryset(self):
-        # Dummy data creation
-        Case.objects.create(title='Case 1',
-                            description='Description of Case 1')
-        Case.objects.create(title='Case 2',
-                            description='Description of Case 2')
-        Case.objects.create(title='Case 3',
-                            description='Description of Case 3')
 
         return Case.objects.all()
 
 
-@require_POST
-def delete_case(request, id):
-    case = Case.objects.get(id=id)
-    case.delete()
-    return redirect('cases:case_list')  # name of your case list URL
+class CaseCreateView(CreateView):
+    model = Case
+    fields = ['title', 'description']
+    template_name = 'cases_service/create_case.html'
+    success_url = reverse_lazy('cases:case_list')
 
 
 class IndexView(generic.ListView):
@@ -84,3 +79,10 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+@require_POST
+def delete_case(request, id):
+    case = Case.objects.get(id=id)
+    case.delete()
+    return redirect('cases:case_list')  # name of your case list URL
