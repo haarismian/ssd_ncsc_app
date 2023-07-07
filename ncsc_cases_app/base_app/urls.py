348e1c707a3fdeea7b7c728c4a4385763aec9245
haarismian <mail@haarismian.com>
2023-07-06 22:01:48 -0400

    added failed login monitoring

--- ncsc_cases_app/base_app/urls.py
+++ ncsc_cases_app/base_app/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 app_name = "base_app"
 urlpatterns = [
@@ -8,7 +9,21 @@ urlpatterns = [
     path("cases/", include("cases_service.urls")),
     path("cvd/", include("cvd_service.urls")),
     path("admin/", admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/',
+         auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/',
+         auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
     path('register/', views.register, name='register'),
 
 
--- ncsc_cases_app/base_app/views.py
+++ ncsc_cases_app/base_app/views.py
@@ -1,6 +1,30 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView as AuthLoginView
+import logging
 from django.shortcuts import render, redirect
+
+logger = logging.getLogger(__name__)
+
+
+class LoginView(AuthLoginView):
+    def form_invalid(self, form):
+        logger.warning(
+            'Failed login attempt - Username: %s, IP address: %s',
+            self.request.POST['username'],
+            get_client_ip(self.request)  # function to get client IP
+        )
+        return super().form_invalid(form)
+
+
+def get_client_ip(request):
+    """Get client IP from request"""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]  # if it is a list, take first IP
+    else:
+        ip = request.META.get('REMOTE_ADDR')  # else take IP provided by Django
+    return ip
 
 
 def index(request):
