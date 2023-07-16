19405e1e76f65fed3e13eda7efe52d3071b609ca
haarismian <mail@haarismian.com>
2023-07-15 22:56:18 -0400

    trying to get cvd form to work

--- ncsc_cases_app/cvd_service/views.py
+++ ncsc_cases_app/cvd_service/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django import forms
+
 
 from django.urls import reverse_lazy
 from django.views import generic
